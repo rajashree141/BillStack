a6369cc05cfdc8bdecc823c1bd9466447b1202ad
rajahree141 <rajashriparwate@gmail.com>
2026-05-02 18:42:26 +0530

    Tailwind installed and added into project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
 
   return (
-    <div>
-      This is the main page
-    </div>
+    <>
+
+      <div className='text-2xl text-red-700'>
+        App
+      </div>
+
+      
+    </>
+
+
+
   )
 }
 
